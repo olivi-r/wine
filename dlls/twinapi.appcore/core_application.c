031b93d93e5f21e844e63a115044ee3530c20c8f
Olivia Ryan <olivia.r.dev@gmail.com>
2026-08-28 09:29:37 +0100

    twinapi.appcore: Implement Windows.Application.Core.CoreApplication::Run.

--- dlls/twinapi.appcore/core_application.c
+++ dlls/twinapi.appcore/core_application.c
@@ -24,6 +24,7 @@ WINE_DEFAULT_DEBUG_CHANNEL(twinapi);
 struct factory
 {
     IActivationFactory IActivationFactory_iface;
+    ICoreApplication ICoreApplication_iface;
     LONG ref;
 };
 
@@ -45,6 +46,13 @@ static HRESULT WINAPI activation_factory_QueryInterface( IActivationFactory *ifa
     {
         IActivationFactory_AddRef( &impl->IActivationFactory_iface );
         *out = &impl->IActivationFactory_iface;
+        return S_OK;
+    }
+
+    if (IsEqualGUID( iid, &IID_ICoreApplication ))
+    {
+        ICoreApplication_AddRef( &impl->ICoreApplication_iface );
+        *out = &impl->ICoreApplication_iface;
         return S_OK;
     }
 
@@ -106,9 +114,94 @@ static const struct IActivationFactoryVtbl activation_factory_vtbl =
     activation_factory_ActivateInstance,
 };
 
+DEFINE_IINSPECTABLE( core_application, ICoreApplication, struct factory, IActivationFactory_iface )
+
+static HRESULT WINAPI core_application_get_Id( ICoreApplication *iface, HSTRING *value )
+{
+    FIXME( "iface %p, value %p stub!\n", iface, value );
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI core_application_eventadd_Suspending( ICoreApplication *iface, IEventHandler_SuspendingEventArgs *handler, EventRegistrationToken *token )
+{
+    FIXME( "iface %p, handler %p, token %p stub!\n", iface, handler, token );
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI core_application_eventremove_Suspending( ICoreApplication *iface, EventRegistrationToken token )
+{
+    FIXME( "iface %p, token %p stub!\n", iface, &token );
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI core_application_eventadd_Resuming( ICoreApplication *iface, IEventHandler_IInspectable *handler, EventRegistrationToken *token )
+{
+    FIXME( "iface %p, handler %p, token %p stub!\n", iface, handler, token );
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI core_application_eventremove_Resuming( ICoreApplication *iface, EventRegistrationToken token )
+{
+    FIXME( "iface %p, token %p stub!\n", iface, &token );
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI core_application_get_Properties( ICoreApplication *iface, IPropertySet **value )
+{
+    FIXME( "iface %p, value %p stub!\n", iface, value );
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI core_application_GetCurrentView( ICoreApplication *iface, ICoreApplicationView **value )
+{
+    FIXME( "iface %p, value %p stub!\n", iface, value );
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI core_application_Run( ICoreApplication *iface, IFrameworkViewSource *view_source )
+{
+    IFrameworkView *view;
+    HRESULT hr;
+
+    TRACE( "iface %p, view_source %p.\n", iface, view_source );
+
+    if (FAILED(hr = IFrameworkViewSource_CreateView( view_source, &view ))) return hr;
+    hr = IFrameworkView_Run( view );
+    IFrameworkView_Release( view );
+    return hr;
+}
+
+static HRESULT WINAPI core_application_RunWithActivationFactories( ICoreApplication *iface, IGetActivationFactory *factory )
+{
+    FIXME( "iface %p, factory %p stub!\n", iface, factory );
+    return E_NOTIMPL;
+}
+
+static const struct ICoreApplicationVtbl core_application_vtbl =
+{
+    core_application_QueryInterface,
+    core_application_AddRef,
+    core_application_Release,
+    /* IInspectable methods */
+    core_application_GetIids,
+    core_application_GetRuntimeClassName,
+    core_application_GetTrustLevel,
+    /* ICoreApplication methods */
+    core_application_get_Id,
+    core_application_eventadd_Suspending,
+    core_application_eventremove_Suspending,
+    core_application_eventadd_Resuming,
+    core_application_eventremove_Resuming,
+    core_application_get_Properties,
+    core_application_GetCurrentView,
+    core_application_Run,
+    core_application_RunWithActivationFactories,
+};
+
 static struct factory factory =
 {
     {&activation_factory_vtbl},
+    {&core_application_vtbl},
     1,
 };
 
